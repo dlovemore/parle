313a7229f7940c27c5fecfc8547d650e8924e9df
David Lovemore <davidlovemore@gmail.com>
2020-04-14 15:36:42 +0100

    Remove old fun code

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from pathlib import Path
 sys.path.insert(0,str(Path(__file__).parent))
 from parle.func import *
 from parle.sym import *
-from parle.fun import *
 from parle.save import *
 from parle.table import *
 from parle.files import *
